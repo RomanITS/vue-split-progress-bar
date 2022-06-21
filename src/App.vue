d9e3815db2f29aa0ba00fac126628f1ea23f11fb
ww <web.west5@gmail.com>
2022-06-21 11:39:39 +0300

    [*] init project

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<!-- npm run serve -->
+<template>
+  <div id="app">
+    <ProgressBar
+      v-for="(item, index) in data"
+      :key="index"
+      :label="item.label"
+      :max="200"
+      :data="item.elementData"
+    />
+  </div>
+</template>
+
+<script>
+import ProgressBar from "../src/components/ProgressBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProgressBar,
+  },
+  data() {
+    return {
+      data: [
+        {
+          label: "Risk of Loss",
+          elementData: [
+            {
+              style: "low",
+              name: "Low",
+              value: 1,
+            },
+            {
+              style: "middle",
+              name: "Middle",
+              value: 78,
+            },
+            {
+              style: "high",
+              name: "High",
+              value: 120,
+            },
+          ],
+        },
+        {
+          label: "Satisfaction",
+          elementData: [
+            {
+              style: "low",
+              name: "Low",
+              value: 1,
+            },
+            {
+              style: "middle",
+              name: "Middle",
+              value: 8,
+            },
+            {
+              style: "high",
+              name: "High",
+              value: 10,
+            },
+          ],
+        },
+        {
+          label: "Impact of Loss",
+          elementData: [
+            {
+              style: "low",
+              name: "Low",
+              value: 40,
+            },
+            {
+              style: "middle",
+              name: "Middle",
+              value: 144,
+            },
+            {
+              style: "high",
+              name: "High",
+              value: 16,
+            },
+          ],
+        },
+        {
+          elementData: [
+            {
+              style: "low",
+              name: "Low",
+              value: 140,
+            },
+            {
+              style: "#bee5eb",
+              name: "Middle",
+              value: 40,
+            },
+            {
+              style: "",
+              name: "High",
+              value: 16,
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,287 @@
+<template>
+  <div class="progress" :class="{ active: showInfo }">
+    <!-- Заголовок progress -->
+    <p v-if="label" class="progress__name">{{ label }}</p>
+    <div class="progress__block">
+      <!-- Всплывающая подсказка progress bar -->
+      <div ref="progress-popover" class="progress__popover">
+        <!-- Название и проценты progress bar -->
+        <span
+          class="progress__popover-title"
+          :class="{
+            ['progress__popover-title_' + style]: styles.includes(style)
+          }"
+          :style="customStyle"
+        >
+          {{ name }} — {{ percent }}
+        </span>
+        <!-- Значение progress bar -->
+        <span class="progress__popover-count">{{ value }} employees</span>
+      </div>
+
+      <div ref="progress-bar" class="progress__bar">
+        <!-- Список progress bar -->
+        <div
+          v-for="(item, index) in formattedOptions"
+          :key="index"
+          class="progress__color"
+          :style="item.customStyle"
+          :class="{
+            ['progress__color_' + item.style]: styles.includes(item.style)
+          }"
+          @mouseover="hoverProgressBlock($event, item), (showInfo = true)"
+          @mouseout="showInfo = false"
+        ></div>
+      </div>
+    </div>
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProgressBar',
+  props: {
+    /** Данные для заполнения progress */
+    data: {
+      type: [Object, Array],
+      default: null,
+      required: true
+    },
+    /** Название progress */
+    label: {
+      type: String,
+      default: '',
+      required: false
+    },
+    /** Максимальное значение progress */
+    max: {
+      type: Number,
+      default: 100
+    },
+    options: {
+      type: Object,
+      default: () => {return {}}
+    }
+  },
+  data () {
+    return {
+      /** Стандартный массив стилей */
+      styles: this.option?.styles || ['low', 'middle', 'high', 'none'],
+
+      /** Контроль видимости подсказки */
+      showInfo: this.option?.showInfo || false,
+
+      /** Значение процентов для подсказки */
+      percent: this.option?.percent || '',
+
+      /** Значение количества для подсказки */
+      value: this.option?.value || '',
+
+      /** Значение названия для подсказки */
+      name: this.option?.name || '',
+
+      /** Значение стиля для подсказки */
+      style: this.option?.style || '',
+
+      /** Переменная для пользовательских стилей progress bar */
+      customStyle: this.option?.customStyle || {}
+    }
+  },
+  computed: {
+    /** Вычисляем данных для progress bar (проценты, значение Not Rated, рандомный цвет) */
+    formattedOptions () {
+      let localSum = 0
+      /** Переобразрвание данных для progress bar */
+      let formatOptions = this.data.map(el => {
+        localSum += el.value
+        const obj = {
+          ...el,
+          customStyle: {
+            width: (el.value / this.max) * 100 + '%'
+          },
+          percent: (el.value / this.max) * 100 + '%',
+          value: el.value
+        }
+
+        /**
+         * В случае когда поле стиля пустое будет сгенерирован рандомный цвет progress bar
+         */
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+
+        if (!this.styles.includes(el.style)) {
+          obj.customStyle.backgroundColor = !el.style
+            ? '#' + randomColor
+            : el.style
+        }
+
+        return obj
+      })
+
+      /** Вычисление поля Not Rated */
+      if (localSum < this.max) {
+        formatOptions.push({
+          style: 'none',
+          name: 'Not Rated',
+          customStyle: {
+            width: ((this.max - localSum) / this.max) * 100 + '%'
+          },
+          percent: ((this.max - localSum) / this.max) * 100 + '%',
+          value: this.max - localSum
+        })
+      }
+      return formatOptions
+    }
+  },
+  methods: {
+    /**
+     * Метод формирования данных и стилей для всплывающей подсказки.
+     * Отдает заголовок, проценты, значение, стиль для выбранного progress bar.
+     * @param {Event} e - Дескриптор события.
+     * @param {Object} item - Данные progress bar.
+     */
+    hoverProgressBlock (e, { percent, style, customStyle, name, value }) {
+      this.percent = percent
+      this.style = style
+      /** Определение стиля для всплывающей подсказки */
+      if (!this.styles.includes(style)) {
+        this.customStyle.color = style || customStyle.backgroundColor
+      } else {
+        delete this.customStyle.color
+      }
+      this.name = name
+      this.value = value
+
+      let position = e.target.offsetLeft
+      let popover = this.$refs['progress-popover']
+
+      const progressItem = this.$refs['progress-bar']
+      const progressPopoverWidth = popover.offsetWidth
+      const progressColorWidth = e.target.offsetWidth
+
+      /** Позиционирование всплывающей подсказки */
+      if (position + progressPopoverWidth > progressItem.offsetWidth) {
+        popover.style.left = position + progressColorWidth + 'px'
+        popover.style.transform = 'translate(-100%, -50%)'
+      } else {
+        popover.style.left = position + 'px'
+        popover.style.transform = 'translate(0%, -50%)'
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$low: #45e596;
+$middle: #ffc44c;
+$high: #ff4c4c;
+$none: #f2f2f2;
+$grey: #808080;
+$text: #1A1A1A;
+.overlay {
+  position: absolute;
+  opacity: 0;
+  pointer-events: none;
+  background-color: #fff;
+  transition: 0.3s;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.progress {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  max-width: 20px 0;
+  text-align: left;
+  &.active {
+    .progress {
+      &__popover {
+        opacity: 1;
+        z-index: 11;
+      }
+      &__color_none {
+        background: $grey;
+      }
+      &__name {
+        z-index: 11;
+      }
+    }
+    .overlay {
+      opacity: 0.8;
+    }
+  }
+  &__block {
+    position: relative;
+  }
+  &__name {
+    margin-right: 30px;
+    min-width: 120px;
+    font-size: 16px;
+  }
+  &__bar {
+    display: flex;
+    overflow: hidden;
+    width: 274px;
+    height: 6px;
+    border-radius: 4px;
+    background: $none;
+    position: relative;
+  }
+  &__color {
+    height: 100%;
+    transition: 0.3s;
+    min-width: 8px;
+    cursor: pointer;
+    &:hover {
+      z-index: 11;
+    }
+    &_low {
+      background: $low;
+    }
+    &_middle {
+      background: $middle;
+    }
+    &_high {
+      background: $high;
+    }
+    &_none {
+      background: $none;
+    }
+    &:not(:last-child) {
+      margin-right: 1px;
+    }
+  }
+  &__popover {
+    white-space: nowrap;
+    opacity: 0;
+    transition: 0.3s;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    &-title {
+      display: block;
+      font-weight: bold;
+      &_low {
+        color: $low;
+      }
+      &_middle {
+        color: $middle;
+      }
+      &_high {
+        color: $high;
+      }
+      &_none {
+        color: $grey;
+      }
+    }
+  }
+}
+</style>
+
